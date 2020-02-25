37346df53d11d0040f0855ef0bd9bb1de5d4666b
IGOR <qrdej31@gmail.com>
2020-02-25 22:17:35 +0100

    Add project files.

---
+++ TravelAgencies/libs.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <fstream>
+#include <optional>
+#include <memory>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <optional>
+#include <sstream>
+
+#include "json.hpp"
+#include "sqlite3.h"
+
+using json = nlohmann::json;
